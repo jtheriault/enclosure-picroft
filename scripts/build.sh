1eca0944952c5341b53cd8a4c03072cefeb4c7ae
Joe Theriault <joseph@yankoismedia.com>
2017-03-10 23:35:25 -0500

    Script building of picroft image
    Configure Travis to build and deploy

---
+++ scripts/build.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# 0. Handle special case of interactive use
+if [ -z $RASPBIAN_ZIP_URL ]; then
+    read -p "URL of Raspbian zip file: " RASPBIAN_ZIP_URL 
+fi
+
+if [ -z $PICROFT_VERSION ]; then
+    read -p "Version being built: " PICROFT_VERSION
+fi
+
+if [ "$(basename $(pwd))" = "build" ] || [ "$(basename $(pwd))" = "scripts" ]; then
+    cd ..
+fi
+
+# 1. Prepare workspace
+mkdir -p build
+cd build
+
+# 2. Get Raspbian image into place
+curl -o raspbian-jessie-lite.zip $RASPBIAN_ZIP_URL
+unzip raspbian-jessie-lite.zip
+mv *.img raspbian.img
+
+# 3. Mount Raspbian image
+SECTOR_START=$(fdisk -l raspbian.img | grep Linux | xargs | cut -d " " -f2)
+let "OFFSET = $SECTOR_START * 512"
+mkdir raspbian
+sudo mount -v -o offset=$OFFSET -t ext4 raspbian.img ./raspbian
+
+# 4. copy folders into image
+sudo cp -R ../etc/* raspbian/etc/
+sudo cp -R ../home/* raspbian/home/
+
+# 5. unmount image
+sync
+sudo umount raspbian
+
+# 6. Name build
+mv raspbian.img picroft-$PICROFT_VERSION.img
+zip picroft-$PICROFT_VERSION.img.zip picroft-$PICROFT_VERSION.img
+
